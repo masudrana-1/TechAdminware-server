eb17395fa356685df88af846f66f1686c18a6566
masudrana-1 <masudrana58855@gmail.com>
2022-10-25 18:34:01 +0600

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.port || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('tech api is running');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
