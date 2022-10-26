bf4e245f4ef07352bf7e8767330db256f99301ed
masudrana-1 <masudrana58855@gmail.com>
2022-10-26 23:50:21 +0600

    remove category

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const port = process.env.port || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json');
+// const category = require('./data/category.json');
+const course = require('./data/courses.json');
+const e = require('express');
 
 app.get('/', (req, res) => {
     res.send('tech api is running');
 })
 
-app.get('/courses', (req, res) => {
-    res.send(courses);
+
+app.get('/course', (req, res) => {
+    res.send(course);
 })
 
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const seletedCourse = course.find(c => c._id === id);
+    res.send(seletedCourse);
+})
 
 app.listen(port, () => {
     console.log('techadminware server running on this port', port);
